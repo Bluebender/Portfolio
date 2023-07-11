408c337109b903f9066f1c386ab16cf4d23b49c6
Vincent Ollivier <olliviev@gmail.com>
2023-07-11 15:23:48 +0200

    Ajout de l'animation des bargraphe pour les Skills

--- assets/js/index.js
+++ assets/js/index.js
@@ -85,4 +85,51 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     })
 
-})
+
+
+
+
+
+    // Animation des graphiques des Skills
+
+
+    // Sélection des sections avec la class "skill"
+    let skillSections = document.getElementsByClassName("skill");
+
+    // Animation des pourcentages
+    for(let skillSection of skillSections){
+        let skillSectionPourcentage = skillSection.getAttribute("data-percent");
+        let skillSectionPourcentageBrut = skillSectionPourcentage.substring(0,2);
+        let n = skillSectionPourcentageBrut; // Nombre final du compteur
+        let cpt = 20; // Initialisation du compteur
+        let duree = 5; // Durée en seconde pendant laquel le compteur ira de 0 à 15
+        let delta = Math.ceil((duree * 1000) / n); // On calcule l'intervalle de temps entre chaque rafraîchissement du compteur (durée mise en milliseconde)
+        let node =  skillSection; // On récupère notre noeud où sera rafraîchi la valeur du compteur
+        let lancementAnimation = false
+    
+        function countdown() {
+            node.setAttribute("data-percent", ++cpt +1 + "%");
+            node.setAttribute("style", "width: "+ ++cpt +"%");
+            
+            if( cpt < n ) { // Si on est pas arrivé à la valeur finale, on relance notre compteur une nouvelle fois
+                setTimeout(countdown, delta);
+            }
+        }
+
+
+        // Ajoutez un écouteur d'événement au défilement de la fenêtre
+        window.addEventListener("scroll", function() {
+            // Vérifiez si la section Skills est visible dans la fenêtre
+            const sectionBounds = skillSection.getBoundingClientRect();
+            const windowHeight = window.innerHeight;
+
+            // Si la section est visible, exécutez la fonction handleScroll
+            if (sectionBounds.bottom < windowHeight && sectionBounds.top > 0 && lancementAnimation === false) {
+                setTimeout(countdown, delta);
+                lancementAnimation = true;
+            }
+        });
+
+    }
+
+});
